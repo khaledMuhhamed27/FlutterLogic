8cd0468776862740bf9c2215af0fac95d05335e3
khaledMuhhamed27 <muhammedxaled40@gmail.com>
2025-04-22 00:01:26 +0300

    as

--- lib/auth/login/screen/login_screen.dart
+++ lib/auth/login/screen/login_screen.dart
@@ -25,139 +25,157 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // email Title
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Email",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
+        padding: const EdgeInsets.only(right: 20.0, left: 20.0, bottom: 70),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // login logo
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.contain,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                ),
+              ),
+              // email Title
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "Email",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
-            ),
-            // email input
-            FormFieldClass(
-              controller: emailController,
-              hittile: "Ex@gmail.com",
-            ),
-            // space
-            SizedBox(height: 20),
-            // password Title
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Password",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
+              // email input
+              FormFieldClass(
+                controller: emailController,
+                hittile: "Ex@gmail.com",
+              ),
+              // space
+              SizedBox(height: 20),
+              // password Title
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "Password",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
-            ),
-            // password
-            TextFormField(
-              // controller]
-              controller: passwordController,
+              // password
+              TextFormField(
+                // controller]
+                controller: passwordController,
 
-              // Style
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isVisibelity = !isVisibelity;
-                    });
-                  },
-                  icon: isVisibelity
-                      ? Icon(
-                          Icons.visibility_off,
-                          color: Colors.blue,
-                        )
-                      : Icon(Icons.visibility, color: Colors.blue),
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 0.2,
-                      color: Colors.blue,
-                    )),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 0.2,
-                      color: Colors.blue,
-                    )),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 0.5,
-                      color: Colors.blue,
-                    )),
-                errorBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 0.2,
-                      color: Colors.blue,
-                    )),
-              ),
-              // cureser
-              cursorColor: Colors.blueAccent,
-              // obscure
-              obscureText: isVisibelity,
-            ),
-            // space
-            SizedBox(height: 50),
-            // button
-            MaterialButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              minWidth: MediaQuery.of(context).size.width * 90,
-              padding: EdgeInsets.all(12),
-              color: Colors.blue,
-              onPressed: () {
-                print("User Email IS : ${emailController.text}");
-                print("User Password IS : ${passwordController.text}");
-              },
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            // space
-            SizedBox(height: 50),
-            //  jump to register screeen
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RegisterScreen(),
+                // Style
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.blue.shade200,
                   ),
-                );
-              },
-              child: Text(
-                "You don't have an account! Click to Register",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.black45),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isVisibelity = !isVisibelity;
+                      });
+                    },
+                    icon: isVisibelity
+                        ? Icon(
+                            Icons.visibility_off,
+                            color: Colors.blue,
+                          )
+                        : Icon(Icons.visibility, color: Colors.blue),
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 0.2,
+                        color: Colors.blue,
+                      )),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 0.2,
+                        color: Colors.blue,
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 0.5,
+                        color: Colors.blue,
+                      )),
+                  errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 0.2,
+                        color: Colors.blue,
+                      )),
+                ),
+                // cureser
+                cursorColor: Colors.blueAccent,
+                // obscure
+                obscureText: isVisibelity,
               ),
-            )
-          ],
+              // space
+              SizedBox(height: 50),
+              // button
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                minWidth: MediaQuery.of(context).size.width * 90,
+                padding: EdgeInsets.all(12),
+                color: Colors.blue,
+                onPressed: () {
+                  print("User Email IS : ${emailController.text}");
+                  print("User Password IS : ${passwordController.text}");
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              // space
+              SizedBox(height: 50),
+              //  jump to register screeen
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "You don't have an account! Click to Register",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black45,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
